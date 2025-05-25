f46a83731748a4e059512cecf6bf55d5dba1939a
zhaokefei <546109656@qq.com>
2025-05-25 11:43:21 +0800

    fix: proxy view func name

--- reef/api/proxy.py
+++ reef/api/proxy.py
@@ -8,13 +8,13 @@ router = APIRouter(prefix="/proxy", tags=["proxy"])
 
 
 @router.post("/")
-async def proxy_request(url: str, request: Request):
+async def proxy_request_post(url: str, request: Request):
     data = await request.json()
     proxy_core = ProxyCore(url=url, method="POST")
     return await proxy_core.dispatch(data)
 
 
 @router.get("/")
-async def proxy_request(url: str):
+async def proxy_request_get(url: str):
     proxy_core = ProxyCore(url=url, method="GET")
     return await proxy_core.dispatch(data={})
